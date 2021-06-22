d22981d4d9805df515e54d53a58af22ffc85f5b9
alexkkm <alexkong0222@gmail.com>
2021-06-22 17:47:50 +0800

    notification done

--- lib/notification/main.dart
+++ lib/notification/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-// ignore: unused_import
-import 'package:timezone/data/latest.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
+import 'local_notification_demo.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,89 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('change-page'),
+          title: Text('Local Notification'),
         ),
-        body: HomePage(),
-      ),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  @override
-  void initState() {
-    super.initState();
-    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
-    var initializationSettingsAndroid =
-        AndroidInitializationSettings('app_icon');
-    var initializationSettingsIOS = IOSInitializationSettings();
-    var initializationSettings = InitializationSettings(
-        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
-    flutterLocalNotificationsPlugin.initialize(
-      initializationSettings,
-    );
-  }
-
-  Future<void> _showNotification(String title, String content) async {
-    const AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails(
-            'your channel id', 'your channel name', 'your channel description',
-            importance: Importance.max,
-            priority: Priority.high,
-            ticker: 'ticker');
-    const NotificationDetails platformChannelSpecifics =
-        NotificationDetails(android: androidPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin
-        .show(0, title, content, platformChannelSpecifics, payload: 'item x');
-  }
-
-  Future<void> _zonedScheduleNotification(
-      int numberOfSeconds, String title, String content) async {
-    await flutterLocalNotificationsPlugin.zonedSchedule(
-        0,
-        title,
-        content,
-        tz.TZDateTime.now(tz.local).add(Duration(seconds: numberOfSeconds)),
-        const NotificationDetails(
-            android: AndroidNotificationDetails('your channel id',
-                'your channel name', 'your channel description')),
-        androidAllowWhileIdle: true,
-        uiLocalNotificationDateInterpretation:
-            UILocalNotificationDateInterpretation.absoluteTime);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          TextButton(
-            child: Text("Single Notification"),
-            onPressed: () async {
-              await _showNotification(
-                  "Notification Title", "Notification Content");
-              debugPrint("Pressed Single Notification");
-            },
-          ),
-          TextButton(
-            child: Text("Notification"),
-            onPressed: () async {
-              await _zonedScheduleNotification(
-                  5, "Scheduled Title", "Scheduled Content");
-              debugPrint("Pressed Scheduled");
-            },
-          ),
-        ],
+        body: LocalNotificationDemo(),
       ),
     );
   }
