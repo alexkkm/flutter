d55e2376a57bcf60b039f58206315e8ab3a00496
alexkkm <alexkong0222@gmail.com>
2021-06-23 14:56:06 +0800

    restructure all folders and files

--- lib/sharing data/main.dart
+++ lib/sharing data/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'share_service.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -70,5 +70,38 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+}
+
+/// This service is responsible for talking with the OS to see if anything was
+/// shared with the application.
+class ShareService {
+  void Function(String)? onDataReceived;
+
+  ShareService() {
+    // If sharing causes the app to be resumed, we'll check to see if we got any
+    // shared data
+    SystemChannels.lifecycle.setMessageHandler((msg) async {
+      if (msg?.contains("resumed") ?? false) {
+        getSharedData().then((String data) {
+          // Nothing was shared with us :(
+          if (data.isEmpty) {
+            return;
+          }
+
+          // We got something! Inform our listener.
+          onDataReceived?.call(data);
+        });
+      }
+      return;
+    });
+  }
+
+  /// Invoke a method on our platform, telling it to give us any shared data
+  /// it has
+  Future<String> getSharedData() async {
+    return await MethodChannel('com.example.flutter_learning')
+            .invokeMethod("getSharedData") ??
+        "";
   }
 }
