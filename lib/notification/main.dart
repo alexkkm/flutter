7a79d1f4c9f6ea3df7bad43ed0ff894b2a5ad47d
alexkkm <alexkong0222@gmail.com>
2021-06-23 11:48:04 +0800

    finish schedule notification function

--- lib/notification/main.dart
+++ lib/notification/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'local_notification_demo.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
 
 void main() {
   runApp(MyApp());
@@ -17,4 +19,67 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+FlutterLocalNotificationsPlugin notificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+class LocalNotificationDemo extends StatefulWidget {
+  @override
+  _LocalNotificationDemoState createState() => _LocalNotificationDemoState();
+}
+
+class _LocalNotificationDemoState extends State<LocalNotificationDemo> {
+  @override
+  void initState() {
+    initializeSetting();
+    tz.initializeTimeZones();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextButton(
+          onPressed: () {
+            createNotification("1", "1 sec notification", "1 sec content",
+                DateTime.now().add(const Duration(seconds: 1)));
+          },
+          child: Text("DateTime"),
+        ),
+        TextButton(
+          onPressed: () {
+            createNotification("1", "Title", "Content",
+                DateTime.now().add(const Duration(seconds: 5)));
+          },
+          child: Text("5 Seconds"),
+        ),
+      ],
+    );
+  }
+
+  //function for creating Notification with specific
+  //channel ID, notification title, notification content, notification trigger time
+  Future<void> createNotification(
+      String channelID, String title, String content, DateTime dateTime) async {
+    notificationsPlugin.zonedSchedule(
+        0,
+        title,
+        content,
+        tz.TZDateTime.from(dateTime, tz.local),
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+              channelID, 'channel name', 'channel description'),
+        ),
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        androidAllowWhileIdle: true);
+  }
+}
+
+void initializeSetting() async {
+  var initializeAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+  var initializeSetting = InitializationSettings(android: initializeAndroid);
+  await notificationsPlugin.initialize(initializeSetting);
 }
