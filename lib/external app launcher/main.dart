d9986ee284ab56de1afee1a148647fca22bd5bf4
alexkkm <alexkong0222@gmail.com>
2021-06-25 11:19:04 +0800

    sucessful tested main.dart

--- lib/external app launcher/main.dart
+++ lib/external app launcher/main.dart
@@ -1,1 +1,46 @@
+import 'package:flutter/material.dart';
 
+import 'package:app_launcher/app_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App Launcher Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  await AppLauncher.openApp(
+                    androidApplicationId: "com.whatsapp",
+                  );
+                },
+                child: Text('Open WhatsApp'),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () async {
+                  final value = await AppLauncher.hasApp(
+                    androidApplicationId: "org.videolan.vlc",
+                  );
+
+                  print(value);
+                },
+                child: Text('Check for VLC'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
