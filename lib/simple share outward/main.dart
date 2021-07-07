50d96d99ab93bd740548193cab14388c53cbe749
alexkkm <alexkong0222@gmail.com>
2021-07-07 12:59:30 +0800

    need to solve the bug

---
+++ lib/simple share outward/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+
+void main() {
+  runApp(DemoApp());
+}
+
+class DemoApp extends StatefulWidget {
+  @override
+  DemoAppState createState() => DemoAppState();
+}
+
+class DemoAppState extends State<DemoApp> {
+  String text = '';
+  List<String> imagePaths = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter",
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Share Plugin'),
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'Share text:',
+                      hintText: 'Enter some text and/or link to share',
+                    ),
+                    maxLines: 2,
+                    onChanged: (String value) => setState(() {
+                      text = value;
+                    }),
+                  ),
+                  const Padding(padding: EdgeInsets.only(top: 12.0)),
+                  Builder(
+                    builder: (BuildContext context) {
+                      return ElevatedButton(
+                        child: const Text('Share'),
+                        onPressed: text.isEmpty && imagePaths.isEmpty
+                            ? null
+                            : () => _onShare(context),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+
+  _onShare(BuildContext context) async {
+    // A builder is used to retrieve the context immediately
+    // surrounding the ElevatedButton.
+    //
+    // The context's `findRenderObject` returns the first
+    // RenderObject in its descendent tree when it's not
+    // a RenderObjectWidget. The ElevatedButton's RenderObject
+    // has its position and size after it's built.
+    final RenderBox box = context.findRenderObject() as RenderBox;
+
+    await Share.share(text,
+        sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+  }
+}
